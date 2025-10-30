ccd238a5a00b3daf852d0b1e8c5f251890224eb5
Samuel <samuel.samuel@computradetech.com>
2025-10-30 12:01:57 +0700

    a

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -27,21 +27,21 @@ log "Pulling latest changes from origin/$BRANCH..."
 git pull origin $BRANCH
 
 log "Building Docker image..."
-sudo docker build -t $IMAGE_NAME .
+docker build -t $IMAGE_NAME .
 
 # Stop and remove any existing container with the same name
 if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
   log "Stopping existing container ($CONTAINER_NAME)..."
-  sudo docker stop $CONTAINER_NAME
+  docker stop $CONTAINER_NAME
 fi
 
 if [ "$(docker ps -a -q -f name=$CONTAINER_NAME)" ]; then
   log "Removing old container ($CONTAINER_NAME)..."
-  sudo docker rm $CONTAINER_NAME
+  docker rm $CONTAINER_NAME
 fi
 
 log "Running new container..."
-sudo docker run -d \
+docker run -d \
   --name $CONTAINER_NAME \
   -p 8080:80 \
   --restart always \
